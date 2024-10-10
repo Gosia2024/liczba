b79982c624dec6aea5ad0f78a94624c67b99aee3
Gosia2024 <lm.sprachen@gmail.com>
2024-10-10 13:08:47 +0200

    czszczenie pola value i === zero

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 function sprawdz(){
     let liczba = document.getElementById("pole").value;
-    
-    if(liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
+    document.getElementById("pole").value= "";
+
+    if(liczba === "") document.getElementById("wynik").innerHTML ="to nie jest liczba";
+    else if(liczba>0) document.getElementById("wynik").innerHTML="dodatnia";
     else if(liczba<0) document.getElementById("wynik").innerHTML="ujemna";
-    else document.getElementById("wynik").innerHTML="zero";
+    else if(liczba==0) document.getElementById("wynik").innerHTML="zero";
+
+    else document.getElementById("wynik").innerHTML = "to nie jest liczba You konw nothing John Snow"
+
     
-}
+} 
